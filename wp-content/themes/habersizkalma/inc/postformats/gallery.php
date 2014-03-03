b94475a35582364573338c9f3b98b1791b8ba77f
alpdogan <tugrulalpdogan@gmail.com>
2014-03-03 08:38:04 +0200

    gallery numbers

--- wp-content/themes/habersizkalma/inc/postformats/gallery.php
+++ wp-content/themes/habersizkalma/inc/postformats/gallery.php
@@ -16,12 +16,19 @@
 	<div class="galleryNumber">
 		<ul>
 			<?php 
-				foreach ($attachment_array as $atKey=>$atVal){
+				$c=1;
+				foreach ($attachment_array as $atKey=>$atVal)
+				{
 					if($imageIndexId==$atKey)
 					{
 						echo "<li class='active'><a href='".get_post_permalink()."&imgId=".$atKey."'>".($atKey+1)."</a></li>";
 					}else{
 						echo "<li><a href='".get_post_permalink()."&imgId=".$atKey."'>".($atKey+1)."</a></li>";
+					}	
+					$c++;					
+					if($c==25){
+						$c=0;
+						echo "<br><br>";
 					}
 				}
 				
@@ -30,60 +37,3 @@
 	</div>
 </div>
 
-<!--
-<div class="post-gallery flex-start flex galleryFlex" data-after="afterCallback" data-startat="<?php echo intval($_GET['i'])==0 ? 0 : intval($_GET['i'])-1; ?>">
-	<ul class="slides">
-	
-	<?php foreach ($attachment_array as $attachment) : ?>
-	    
-	    <?php
-	    		if(is_single()) {
-	    			$src = wp_get_attachment_image_src( $attachment, 'single');
-					} else {
-	        	$src = wp_get_attachment_image_src( $attachment, 'blog'); 
-	        }
-	        $image_url = wp_get_attachment_image_src($attachment,'full'); $image_url = $image_url[0];
-	        array_push($caption_array,get_post_field('post_excerpt', $attachment));
-	    ?>
-	    <li>
-	        <img
-	        src="<?php echo $src[0]; ?>" 
-	        />
-	    </li>
-	<?php endforeach; ?>
-	</ul>
-</div>
-<script type="text/javascript">
-	jQuery(function(){
-		var indexIsDefined = false;
-		jQuery.each(window.location.search.split('&'),function(i,x){
-			if(x.replace(/=[\d+]/g,"")=='i'){
-				indexIsDefined = true;
-			}
-		});
-		if(!indexIsDefined)
-		{
-			var newUrl = window.location.origin + window.location.pathname + window.location.search + "?gallery=1&i=1";
-			window.history.pushState({"html":"","pageTitle":"Resim"},"title", newUrl);
-			setCaption(captions[0]);
-		}else
-		{
-			setCaption(captions[<?php echo intval($_GET['i'])-1 ?>]);
-		}
-
-
-	})
-	window.slideImages = '<?php echo $attachments ?>'.split(',');
-	var captions = <?php echo json_encode($caption_array); ?>;
-	function afterCallback( e ){
-		var newUrl = window.location.origin + window.location.pathname + window.location.search.replace(/i=\d+/g, "i=" + (parseInt(e.currentSlide)+1));
-		setCaption(captions[e.currentSlide]);
-	    window.history.pushState({"html":"","pageTitle":"Resim"},"title", newUrl);
-	}
-
-	function setCaption(text)
-	{
-		jQuery("aside.sidebar .galleryCaption").html('<h4>' + text + '</h4>');
-	}
-</script>
--->
